18f513e9c10070503cf7ac754088d9cf43f90d5b
eric diskin <ericdiskin26@gmail.com>
2019-01-26 21:44:35 -0500

    Second Commit

--- js/gamerender.js
+++ js/gamerender.js
@@ -1,5 +1,19 @@
 var deltaTime
+    ltc = 0,
+    world = {},
+    canv,
+    c;
 
 function gameLoop() {
-    
-}
+    window.requestAnimationFrame(gameLoop);
+
+};
+
+
+
+function _init(e) {
+    canv = $('.canv');
+    c = canv[0].getContext('2d');
+};
+
+window.onload = _init;
